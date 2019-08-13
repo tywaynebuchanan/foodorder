23894b126d81994db1e7d1f2b9b763a58bf08262
Tywyane <tywaynebuchanan@gmail.com>
2019-08-13 17:56:59 -0500

    changes to scale

--- vieworders.php
+++ vieworders.php
@@ -4,6 +4,8 @@
 <html>
 <head>
 	<title>Food Order</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.css.map">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
