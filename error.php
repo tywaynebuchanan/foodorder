7cd986d580bd0fc4524e17a77644a7aff70a144d
Tywyane <tywaynebuchanan@gmail.com>
2019-08-13 18:55:02 -0500

    changes

--- error.php
+++ error.php
@@ -8,7 +8,40 @@
 </head>
 <body>
 
+<nav class="navbar is-info">
+<div class="container">
+<div class="navbar-brand">
+      <a class="navbar-item" href="index.php" style="font-weight:bold;">
+        <img src="images/ordering.png" width="25" height="25">
+        Lunch Ordering System
+      </a>
+      <span class="navbar-burger burger" data-target="navMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+    </div>
+<div id="navMenu" class="navbar-menu">
+<div class="navbar-end">
+        <a href="#" class="navbar-item is-active">Home</a>
+        <a href="vieworders.php" class="navbar-item">View Orders</a>
+        <a href="#" class="navbar-item">About Us</a>
+        
+      </div>
+</div>
+</div>
+</nav>
+<script type="text/javascript">
+      (function() {
+        var burger = document.querySelector('.burger');
+        var nav = document.querySelector('#'+burger.dataset.target);
 
+        burger.addEventListener('click', function(){
+          burger.classList.toggle('is-active');
+          nav.classList.toggle('is-active');
+        });
+      })();
+    </script>
 <section class="hero is-info">
   <div class="hero-body">
     <div class="container">
@@ -24,7 +57,7 @@
 </section>
 
 
-<div class="container">
+<div class="container" style="padding-top: 10px;">
 	<div class="buttons are-medium is-outlined is-info">
   <a class="button" href="index.php">Place order</a>
   
@@ -38,4 +71,16 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </body>
+
+<div style="padding-top: 10px;">
+<footer class="footer" >
+  <div class="content has-text-centered is-info">
+    <p>
+      <strong>Lunch Ordering System</strong> by LADs </a>
+
+    </p>
+  </div>
+</footer>
+<div>
+
 </html>
